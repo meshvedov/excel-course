2596e42aa325287711bb3cc51c9389d2c93a125a
Mikhail Shvedov <micangel@mail.ru>
2020-05-19 23:07:14 +0300

    lesson 35

---
+++ src/components/table/Table.js
@@ -0,0 +1,23 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+
+export class Table extends ExcelComponent {
+    static className = 'excel__table'
+
+    toHTML() {
+        return `
+            <div class="row">
+                <div class="row-info"></div>
+                <div class="row-data">
+                    <div class="column"></div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="row-info">1</div>
+                <div class="row-data">
+                    <div class="cell selected" contenteditable></div>
+                </div>
+            </div>
+        `
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './scss/index.scss';
+import {Excel} from './components/excel/excel';
+import {Formula} from './components/formula/Formula';
+import {Table} from './components/table/Table';
+import {Header} from './components/header/Header';
+import {Toolbar} from './components/toolbar/Toolbar';
 
+const excel = new Excel('#app', {
+    components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render();
